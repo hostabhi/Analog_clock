4cde921830df6a6ebc00a79a55a06f2e13dbb918
hostabhi <123621943+hostabhi@users.noreply.github.com>
2023-07-13 10:57:29 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std ; 
+
+int main(){
+  
+   string a ="";
+
+
+   int n =3 ;
+
+   for(int i = 1 ; i<=n;i++){
+    a+=to_string(i);
+   }
+
+cout<<a<<endl ; 
+
+    return 0 ;
+}
