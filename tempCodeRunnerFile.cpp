d25f54e7f4b6404995f708946634db1449496acc
hostabhi <123621943+hostabhi@users.noreply.github.com>
2023-07-13 11:05:21 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,1 @@
-#include<iostream>
 
-using namespace std ; 
-
-int main(){
-  
-   string a ="";
-
-
-   int n =3 ;
-
-   for(int i = 1 ; i<=n;i++){
-    a+=to_string(i);
-   }
-
-cout<<a<<endl ; 
-
-    return 0 ;
-}
